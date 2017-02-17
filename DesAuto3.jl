6bcf00c76bcc5f04107ee8e9585672bda394ab8f
Daniel Hulse <hulsed@oregonstate.edu>
2017-02-17 01:27:36 -0800

    Progress Point

--- DesAuto3.jl
+++ DesAuto3.jl
@@ -8,35 +8,55 @@ module DesAuto3
   end
 
   function exhaustive(dim,lim)
+
+    optimizer=[]
+    optimum=[]
+    prevloc=rand(dim)
+    loc=rand(dim)
+    minval=[]
+    prevmin=Inf
+    cpt=rand(1)
     for cpt=1:lim
       if dim==1
         funcval=DesAuto3.mishra11(cpt)
         println(cpt,funcval)
       else
-        minval=exhaustiverec(dim,lim,2,cpt,Inf)
+        minval,loc=exhaustiverec(dim,lim,2,cpt,prevmin, prevloc)
+        prevloc=loc
+        prevmin=minval
       end
-      return minval
     end
+    return minval,loc
   end
 
-  function exhaustiverec(dim,lim, curdim, ppt,minval)
+  function exhaustiverec(dim,lim, curdim, ppt,prevmin, prevloc)
       cpt=[ppt;0]
+      loc=prevloc
+      minval=prevmin
+
       for n=1:lim
         cpt[end]=n
         if curdim==dim
           funcval=DesAuto3.mishra11(cpt)
-          println(cpt,funcval)
+          #println(cpt,funcval)
           if funcval<minval
               minval=funcval
+              loc=copy(cpt)
+              prevloc=copy(loc)
+              #println(minval, loc)
+            else
+              loc=prevloc
           end
         else
-          println(curdim, cpt)
-          minval=exhaustiverec(dim, lim, curdim+1, cpt, minval)
+          #println(curdim, cpt)
+          minval,loc=exhaustiverec(dim, lim, curdim+1, cpt, prevmin, prevloc)
+          prevloc=loc
+          prevmin=minval
         end
-
+        #println(loc)
       end
-
-        return minval
+        #println(loc)
+        return minval,loc
   end
 
 end
