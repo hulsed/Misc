5d292af2ce65647d41b51f705ac2b3c7490775fc
Daniel Hulse <hulsed@oregonstate.edu>
2017-10-03 00:21:51 -0700

    created functions for testing

--- Neural Network/ROB537HW1/funcs.py
+++ Neural Network/ROB537HW1/funcs.py
@@ -5,7 +5,9 @@ Created on Sun Oct 01 20:55:46 2017
 @author: HulseDanielE
 """
 import numpy
-import csv    
+import csv   
+import random 
+import matplotlib.pyplot as plt
 #creates Weights for a given layer in the network 
 def createWeights(inputNodes,outputNodes):
     #weight array is inputNodes tall and outputnodes wide
@@ -41,10 +43,11 @@ def pipeWeights(inputs, Ws):
     #initialize outputs
     weightOut=numpy.random.random((outputNum, inputNum))
     #for each input i
-    for i in range(inputNum):
+    for ij in range(int(inputNum)):
         #for each output j
-        for j in range(outputNum):
-            weightOut[j,i]=Ws[i,j]*inputs[i]
+        for jk in range(int(outputNum)):
+            
+            weightOut[jk,ij]=Ws[ij,jk]*inputs[ij]
     return weightOut
 
 
@@ -111,10 +114,10 @@ def backprop(eta,inputData,outputData,hiddenWs,hiddenBs,outputWs,outputBs):
     return hiddenWsnew, hiddenBsnew, outputWsnew, outputBsnew
 
 #def train(eta,TrainIn, TrainOut):
-def getData():
+def getData(datafile):
     InDataMatrix=[]
     OutDataMatrix=[]
-    with open('test1.csv') as DataFile:
+    with open(datafile) as DataFile:
         csvReader=csv.reader(DataFile)
         for row in csvReader:
             InDataMatrix.append(row[0:5])
@@ -122,7 +125,21 @@ def getData():
     
     return InDataMatrix, OutDataMatrix
 
-def test():
+def classify(input):
+    
+    if input[0]==0:
+        if input[1]==1:
+            classification=0
+        else:
+            classification=2
+    else:
+        if input[1]==0:
+            classification=1
+        else:
+            classification=2
+    return classification
+
+def test(datafile):
     #input parameters
     inputNodes=5
     outputNodes=2
@@ -131,30 +148,234 @@ def test():
     inputData=numpy.array([1,1,1,1,1])
     #inputData=numpy.array([0,0,0,0,0])
     outputData=numpy.array([0,1])
-    eta=0.1
-    epochs=100
+    eta=0.15
+    epochs=10
     
     hiddenWs,hiddenBs,outputWs,outputBs=createNetwork(inputNodes, outputNodes, hiddenNodes)
     prediction=predict(inputData,hiddenWs,hiddenBs,outputWs,outputBs)
     
     #get data
-    InDataMatrix, OutDataMatrix=getData()
+    InDataText, OutDataText=getData('train1.csv')
+    samples=len(InDataText)
+    InDataMatrix=numpy.random.random([samples,inputNodes])
+    OutDataMatrix=numpy.random.random([samples,outputNodes])
+    
+    
+    for k in range(samples):
+        inputDat=numpy.array(InDataText[k])
+        InDataMatrix[k,:]=map(float,inputDat)
+        outputDat=numpy.array(OutDataText[k])
+        OutDataMatrix[k,:]=map(float,outputDat)
 
     #train
+    samples=len(InDataMatrix)
+    err=numpy.random.random(outputNodes)
+    
+    MSE=numpy.random.random([epochs,samples])
+    sqerr=numpy.zeros([epochs,samples])
+    classerr=numpy.zeros([epochs,samples])
+    runningerr=numpy.zeros([epochs,samples])
     for i in range(epochs):
-        for j in range(len(InDataMatrix)):
-            inputDat=numpy.array(InDataMatrix[j])
-            inputData=numpy.array(map(float,inputDat))
-            outputDat=numpy.array(OutDataMatrix[j])
-            outputData=numpy.array(map(float,outputDat))
+        #sqerr=numpy.zeros(samples)
+        index=random.sample(range(0,samples),samples)
+        
+        for j in range(samples):
+
+            mark=index[j]
+                
+            inputData=InDataMatrix[mark,:]
+            outputData=OutDataMatrix[mark,:]
+            
             hiddenWs, hiddenBs, outputWs, outputBs= \
             backprop(eta,inputData,outputData,hiddenWs,hiddenBs,outputWs,outputBs)
-    
-    prediction=predict(inputData,hiddenWs,hiddenBs,outputWs,outputBs)
-    return prediction
+            prediction=predict(inputData,hiddenWs,hiddenBs,outputWs,outputBs)
+            err=prediction-outputData
             
-
+            correctclass=classify(outputData)
+            predclass=classify(numpy.round(prediction))
+            
+            if correctclass==predclass:
+                classerr[i,j]=0
+            else:
+                classerr[i,j]=1
+            runningerr[i,j]=sum(sum(classerr))/(i*samples+j+1)
+            
+            
+            sqerr[i,j]=sum(numpy.power(err,2))
+            MSE[i,j]=sum(sum(sqerr))/(i*samples+j+1)
     
+    #validate
+    #get data
+    InDataTextv, OutDataTextv=getData(datafile)
+    samplesv=len(InDataTextv)
+    InDataMatrixv=numpy.random.random([samplesv,inputNodes])
+    OutDataMatrixv=numpy.random.random([samples,outputNodes])
+    
+    
+    for k in range(samples):
+        inputDatv=numpy.array(InDataTextv[k])
+        InDataMatrixv[k,:]=map(float,inputDatv)
+        outputDatv=numpy.array(OutDataTextv[k])
+        OutDataMatrixv[k,:]=map(float,outputDatv)
+    
+    
+    index=random.sample(range(0,samples),samples)
+    classerr2=numpy.zeros(samples)
+    for j in range(samples):
+        mark=index[j]
+        inputDatav=InDataMatrixv[mark,:]
+        outputDatav=OutDataMatrixv[mark,:]
+
+        prediction=predict(inputDatav,hiddenWs,hiddenBs,outputWs,outputBs)
+        err=prediction-outputDatav
+        
+        correctclass=classify(outputDatav)
+        predclass=classify(numpy.round(prediction))
+        
+        if correctclass==predclass:
+            classerr2[i]=0
+        else:
+            classerr2[i]=1
+              
+    validationerr=sum(classerr2)/samples
+            
+    
+    return validationerr
+
+def script():
+    error=numpy.zeros([10,8])
+    for tries in range(10):
+        err=numpy.zeros(8)
+        for i in range(8):
+            mark=i+1
+            err[i]=test(mark)
+        error[tries]=err
+    
+    x=range(1,9)
+    means=numpy.zeros(8)
+    mins=numpy.zeros(8)
+    maxs=numpy.zeros(8)
+    stds=numpy.zeros(8)
+    for i in range(8):
+        
+        means[i]=numpy.mean(error[:,i])
+        stds[i]=numpy.std(error[:,i])
+        mins[i]=min(error[:,i])
+        maxs[i]=max(error[:,i])
+    bars=maxs-mins
+    
+    plt.errorbar(x,means,stds, linestyle='None',marker='+')
+    plt.xlabel('Number of Hidden Nodes')
+    plt.ylabel('Classification Error')
+    plt.title('Effect of Hidden Nodes')
+    return error
+            
+def script2():
+    error=numpy.zeros([10,8])
+    for tries in range(10):
+        err=numpy.zeros(8)
+        for i in range(8):
+            mark=i+1
+            err[i]=test(mark)
+        error[tries]=err
+    
+    x=range(1,9)
+    means=numpy.zeros(8)
+    mins=numpy.zeros(8)
+    maxs=numpy.zeros(8)
+    stds=numpy.zeros(8)
+    for i in range(8):
+        
+        means[i]=numpy.mean(error[:,i])
+        stds[i]=numpy.std(error[:,i])
+        mins[i]=min(error[:,i])
+        maxs[i]=max(error[:,i])
+    bars=maxs-mins
+    
+    plt.errorbar(x,means,stds, linestyle='None',marker='+')
+    plt.xlabel('Number of Epochs Used for Training')
+    plt.ylabel('Classification Error')
+    plt.title('Effect of Training Time')
+    return error
+            
+def script3():
+    error=numpy.zeros([10,8])
+    eta=numpy.linspace(0.01,0.2,8)
+    for tries in range(10):
+        err=numpy.zeros(8)
+        for i in range(8):
+            err[i]=test(eta[i])
+        error[tries]=err
+    
+    x=range(1,9)
+    means=numpy.zeros(8)
+    mins=numpy.zeros(8)
+    maxs=numpy.zeros(8)
+    stds=numpy.zeros(8)
+    for i in range(8):
+        
+        means[i]=numpy.mean(error[:,i])
+        stds[i]=numpy.std(error[:,i])
+        mins[i]=min(error[:,i])
+        maxs[i]=max(error[:,i])
+    bars=maxs-mins
+    
+    plt.errorbar(eta,means,stds, linestyle='None',marker='+')
+    plt.xlabel('Learning Parameter Value')
+    plt.ylabel('Classification Error')
+    plt.title('Effect of Learning Parameter')
+    return error
+            
+def script4():
+    error=numpy.zeros([10,2])
+    err=numpy.zeros(2)
+    for tries in range(10):
+        for i in range(2):
+            err[i]=test(2)
+        error[tries]=err
+    
+    x=range(1,2)
+    means=numpy.zeros(2)
+    mins=numpy.zeros(2)
+    maxs=numpy.zeros(2)
+    stds=numpy.zeros(2)
+    for i in range(2):
+        
+        means[i]=numpy.mean(error[:,i])
+        stds[i]=numpy.std(error[:,i])
+        mins[i]=min(error[:,i])
+        maxs[i]=max(error[:,i])
+    bars=maxs-mins
+    
+    return error
+
+def script5():
+    error=numpy.zeros([10,8])
+    data=['test1.csv','test2.csv','test3.csv']
+    for tries in range(10):
+        err=numpy.zeros(8)
+        for i in range(3):
+            err[i]=test(data[i])
+        error[tries]=err
+    
+    x=range(1,4)
+    means=numpy.zeros(3)
+    mins=numpy.zeros(3)
+    maxs=numpy.zeros(3)
+    stds=numpy.zeros(3)
+    for i in range(3):
+        
+        means[i]=numpy.mean(error[:,i])
+        stds[i]=numpy.std(error[:,i])
+        mins[i]=min(error[:,i])
+        maxs[i]=max(error[:,i])
+    bars=maxs-mins
+    
+    plt.errorbar(x,means,stds, linestyle='None',marker='+')
+    plt.xlabel('Test Data Set')
+    plt.ylabel('Classification Error')
+    plt.title('Generalization Across Data Sets')
+    return error
     
 
 
