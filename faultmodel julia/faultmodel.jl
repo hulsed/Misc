1c33e93d360ea4bd57a93fcf6ce68a56c0cdc50d
Daniel Hulse <hulsed@oregonstate.edu>
2019-10-30 16:48:10 -0700

    run one fault now possible (who knows if it works...)

--- faultmodel julia/faultmodel.jl
+++ faultmodel julia/faultmodel.jl
@@ -2,38 +2,38 @@
 include("faultmodeler.jl")
 #using .faultmodeler
 
-impEEmodes=Dict(mode("no_v", "moderate","major"), mode("inf_v", "rare","major"))
+impEEmodes=Dict(mode(:no_v, :moderate,:major), mode(:inf_v, :rare,:major))
 importEE = finit(:Import_EE, :importEE!, Dict(:EE_1=>:EEout),impEEmodes, Dict(:ET=>1.0))
 function importEE!(f::fxn, t::Float64)
-  if f.flows["EEout"]["rate"] > 5.0 push!(f.modes, "no_v")
+  if f.flows[:EEout][:rate] > 5.0 push!(f.modes, :no_v)
 
-  if in("no_v",f.modes)       f.states[:ET]=0.0
-  elseif in("inf_v", f.modes) f.states[:ET]=100.0
+  if in(:no_v,f.modes)       f.states[:ET]=0.0
+  elseif in(:inf_v, f.modes) f.states[:ET]=100.0
   else                        f.states[:ET]=1.0 end
   f.flows[:EEout][:effort]=f.states[:ET]
   end
 end
 
-importWater = finit(:Import_Water, :importWater!, Dict(:Wat_1=>:Watout), Dict(mode("no_wat", "moderate", "major")))
+importWater = finit(:Import_Water, :importWater!, Dict(:Wat_1=>:Watout), Dict(mode(:no_wat, :moderate, :major)))
 function importWater!(f::fxn, t::Float64)
-  if in("no_wat", f.modes)  f.flows[:Watout][:level]=0.0
+  if in(:no_wat, f.modes)  f.flows[:Watout][:level]=0.0
   else                      f.flows[:Watout][:level]=1.0 end
 end
 
-exportWater = finit(:Export_Water, :exportWater!, Dict(:Wat_1=>:Watin), Dict(mode("block", "moderate", "major")))
+exportWater = finit(:Export_Water, :exportWater!, Dict(:Wat_2=>:Watin), Dict(mode(:block, :moderate, :major)))
 function exportWater!(f::fxn, t::Float64)
-  if in("block", f.modes)   f.flows[:Watin][:area]=0.0 end
+  if in(:block, f.modes)   f.flows[:Watin][:area]=0.0 end
 end
 
-importSig = finit(:Import_Signal, :importSig!, Dict(:Sig_1=>:Sigout), Dict(mode("no_sig", "moderate", "major")))
+importSig = finit(:Import_Signal, :importSig!, Dict(:Sig_1=>:Sigout), Dict(mode(:no_sig, :moderate, :major)))
 function importSig!(f::fxn, t::Float64)
-  if in("nosig", f.modes) f.flows[:Sigout][:power]=0.0
+  if in(:nosig, f.modes) f.flows[:Sigout][:power]=0.0
   elseif t<5.0  f.flows[:Sigout][:power]=0.0
   elseif t<50   f.flows[:Sigout][:power]=1.0
   else          f.flows[:Sigout][:power]=0.0 end
 end
 
-moveWatmodes=Dict(mode("mech_break", "moderate", "major"), mode("short", "rare", "major"))
+moveWatmodes=Dict(mode(:mech_break, :moderate, :major), mode(:short, :rare, :major))
 moveWatflows=Dict(:EE_1=>:EEin, :Sig_1=>:Sigin, :Wat_1=>:Watin, :Wat_2=>:Watout)
 moveWat = finit(:Move_Water, :moveWat!, moveWatflows,moveWatmodes, Dict(:eff=>1.0, :rs=>1.0), [:t1] )
 function moveWat!(f::fxn, t::Float64)
@@ -68,7 +68,23 @@ flows=Dict( :EE_1=>Dict(:rate=>1.0, :effort=>1.0),
 
 initfxns = [importEE, importWater, exportWater, importSig, moveWat]
 
-initmdl = init_mdl(flows, initfxns)
+pump = initialize_model(:pump, flows, initfxns)
+
+#graph, nodelabels = make_graph(initmdl)
+
+plot_graph(pump.graph, pump.nodelabels)
+
+struct runparameters
+  times::Array
+  timestep::Float64
+  endtime::Float64
+  comparenominal::Bool
+end
+
+rp = runparameters([0,50],1,50,true)
+
+hist = run_one_fault(pump, :Move_Water, :mech_break, 5.0, rp)
+
 
 #construct function
 #first, add flows
--- faultmodel julia/faultmodeler.jl
+++ faultmodel julia/faultmodeler.jl
@@ -1,9 +1,14 @@
 
-using LightGraphs, MetaGraphs
+using LightGraphs, MetaGraphs, GraphPlot
 
-mutable struct mdl
+mutable struct model
+  name::Symbol
   fxns::Dict
   flows::Dict
+  flowfxns::Dict
+  graph::SimpleGraph
+  nodelabels::Array
+  time::Float64
 end
 
 mutable struct fxn
@@ -26,34 +31,161 @@ struct fparams
   timers::Array
 end
 
+struct scenario
+  name::String
+  joint::Int
+  faults::Dict
+  time::Float64
+  rate::Float64
+end
+
+struct runparameters
+  times::Array
+  timestep::Float64
+  endtime::Float64
+  comparenominal::Bool
+end
+
+
+function construct_scenario(mdl::model, fxnfaults::Dict, time::Float64,jointtype="independent", jointprob=0.1)
+  name=""
+  for func in keys(fxnfaults)
+    name=name*String(func)
+    for modes in fxnfaults[func]
+      name=name*String(modes)*", "
+    end
+  end
+  joint=length(fxnfaults)
+  if joint>1
+    #need to build functions for rate math for joint probabilities
+    #as well as probabilities for times
+    if jointtype=="independent" rate = 1
+    elseif jointtype=="custom"   rate = 2 end
+  elseif !isempty(fxnfaults)
+    rate = mdl.fxns[collect(keys(fxnfaults))[1]].modelist[collect(values(fxnfaults))[1][1]][:rate]
+  else
+    rate=1.0
+  end
+  return scenario(name, joint, fxnfaults, time, rate)
+end
+
+#single fault scenarios
+function construct_scenarios(mdl::model, times)
+  scenlist=[]
+  for func in keys(mdl.fxns)
+    for mode in keys(mdl.fxns[func])
+      for time in times
+        fxnfaults=Dict(func=>mode)
+        push!(scenlist, construct_scenario(mdl,fxnfaults, time))
+      end
+    end
+  end
+  return scenlist
+end
+
+function run_one_fault(mdl::model, fxn::Symbol, fault::Symbol, time::Float64, rp::runparameters)
+  nomscen = construct_scenario(mdl, Dict(), 0.0)
+  nomhist = prop_one_scenario(mdl, nomscen, rp)
+
+  scen = construct_scenario(mdl, Dict(fxn => [fault]), time)
+  hist = prop_one_scenario(mdl, nomscen, rp)
+  #add things to process model later
+  return hist
+end
+
+
+function prop_one_scenario(mdl::model, scen::scenario, rp::runparameters, prevhist=Dict())
+  if isempty(prevhist)
+    timerange=0:rp.timestep:rp.endtime
+    hist = Dict()
+  else
+    timerange=scen.time:rp.timestep:rp.endtime
+    hist = deepcopy(prevhist)
+  end
+  for rtime in timerange
+    hist[rtime] = propagate(mdl, rtime, scen)
+  end
+  return hist
+end
+
+function propagate(mdl::model, rtime::Float64, scen::scenario)
+  mdl=deepcopy(mdl)
+  lastflows=copy(mdl.flows)
+  activefxns=Set(keys(mdl.fxns))
+  nextfxns=Set()
+  #add modes from scenario
+  if rtime==scen.time
+    for func in keys(scen.faults)
+      push!(mdl.fxns[func].modes, scen.faults[func])
+    end
+  end
+  n=1
+  while !isempty(activefxns)
+    for fxnname in copy(activefxns)
+      laststate=copy(mdl.fxns[fxnname].states)
+      lastfaults=copy(mdl.fxns[fxnname].modes)
+      eval(mdl.fxns[fxnname].behav)(mdl.fxns[fxnname], rtime)
+      if laststate!=mdl.fxns[fxnname].states||lastfaults!=mdl.fxns[fxnname].modes
+        push!(nextfxns, fxnname)
+      end
+    end
+    for flowname in keys(mdl.flows)
+      if lastflows[flowname]!=mdl.flows[flowname]
+        for fxnname in mdl.flowfxns[flowname]
+          push!(nexfxns, fxnname)
+        end
+      end
+    end
+    activefxns = copy(nextfxns)
+    empty!(nextfxns)
+    n+=1
+    if n>1000
+      print("Undesired looping in function")
+      print(scen.name)
+      break
+    end
+  end
+return mdl
+end
+
+
+
+
 function finit(name, behav, flows,modes=Dict(), states=Dict(),  timers=[])
   return fparams(name, behav,flows, modes, states, timers)
 end
 
-function mode(name::String, rate::String, rcost::String)
-  return name=>Dict("rate"=>rate, "rcost"=>rcost)
+function mode(name::Symbol, rate::Float64, rcost::Symbol)
+  return name=>Dict(:rate=>rate, :rcost=>rcost)
 end
 
-function init_mdl(flows::Dict, initfxns::Array)
+function initialize_model(name::Symbol, flows::Dict, initfxns::Array)
   fxns=Dict()
   for initfxn in initfxns
     fxnflows=Dict(initfxn.flows[flow]=>flows[flow] for flow in keys(initfxn.flows))
     modes=Set(["nom"])
     timers=Dict(timer=>0.0 for timer in initfxn.timers)
     func=fxn(initfxn.name, initfxn.behav, fxnflows, initfxn.states, initfxn.modes, modes, 0.0, timers)
-    fxns[initfxn.behav]=func
+    fxns[initfxn.name]=func
   end
-  return mdl(fxns,flows)
+  fxnflowmap =Dict(initfxn.name=>keys(initfxn.flows) for initfxn in initfxns)
+  graph,nodes, nodelabels= make_graph(flows, fxnflowmap)
+  flowfxns=Dict(flow=>[nodelabels[i] for i in neighbors(graph,nodes[flow])] for flow in keys(flows))
+  return model(name, fxns,flows,flowfxns, graph, nodelabels, 0.0)
 end
 
-function make_graph(initflows::Dict, initfxns::Array)
-  graph = SimpleGraph()
-  fxnflowmap =Dict(initfxn.name=>keys(initfxn.flows) for initfxn in initfxns)
-  nodes = Dict(map(reverse, collect(enumerate(Iterators.flatten([keys(initflows), keys(fxnflowmap)])))))
-  for fxn in fxnflowmap
-    for flow in fxn
-      add_edge!(graph, nodes[fxn], nodes[flow])
+function make_graph(flows::Dict, fxnflowmap::Dict)
+  nodelabels = collect(Iterators.flatten([keys(flows), keys(fxnflowmap)]))
+  nodes = Dict(map(reverse, collect(enumerate(Iterators.flatten([keys(flows), keys(fxnflowmap)])))))
+  graph = SimpleGraph(length(nodes))
+  for fxnname in keys(fxnflowmap)
+    for flowname in fxnflowmap[fxnname]
+      add_edge!(graph, nodes[fxnname], nodes[flowname])
     end
   end
-  return graph
+  return graph, nodes, nodelabels
+end
+
+function plot_graph(graph, nodelabels)
+  gplot(graph, nodelabel=nodelabels)
 end
