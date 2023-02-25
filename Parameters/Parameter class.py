201838c1da56a941cf1dce31377107834e74274e
Daniel Hulse <daniel.e.hulse@nasa.gov>
2023-02-24 16:52:43 -0800

    Update Parameter class.py

--- Parameters/Parameter class.py
+++ Parameters/Parameter class.py
@@ -9,6 +9,8 @@ from dataclasses import dataclass
 from collections import namedtuple
 
 from recordclass import dataobject, astuple, asdict
+import warnings
+import numpy as np
 
 """
 class Parameter():
@@ -40,12 +42,61 @@ class Parameter(dataobject):
                    raise Exception(varname+" ("+str(var_val)+") outside bounds : ("+str(var_lims))
 
 """
+
 class Parameter(dataobject, readonly=True):
-    def __init__(self,**kwargs):
+    """
+    The Parameter class defines model/function/flow values which are immutable,
+    that is, the same from model instantiation through a simulation. Parameters 
+    inherit from recordclass, giving them a low memory footprint, and use type
+    hints and ranges to ensure parameter values are valid.
+    
+    e.g.,
+    class Param(Parameter, readonly=True):
+        x:          float = 30.0
+        y:          float = 30.0
+        x_lim = (0.0,100.0)
+        y_set = (0.0,30.0,100.0)
+    defines a parameter with float x and y fields with default values of 30 and
+    x_lim minimum/maximum values for x and y_set possible values for y. Note that
+    readonly=True should be set to ensure fields are not changed.
+    
+    This parameter can then be instantiated using:
+        p = Param(x=1.0, y=0.0)
+    """
+    def __init__(self, strict_immutability=True,**kwargs):
+        """
+        Initializes the parameter with given kwargs.
+
+        Parameters
+        ----------
+        strict_immutability : bool
+            Performs basic checks to ensure fields are immutable
+        
+        **kwargs : kwargs
+            Fields to set to non-default values.
+        """
         for k, v in kwargs.items():
             self.check_lim(k,v)
         super().__init__(**kwargs)
+        if strict_immutability: self.check_immutable()
+        self.check_type()
     def check_lim(self, k, v):
+        """
+        Checks to ensure the value v for field k is within the defined limits
+        self.k_lim or set constraints self.k_set
+
+        Parameters
+        ----------
+        k : str
+            Field to check
+        v : mutable
+            Value for the field to check
+
+        Raises
+        ------
+        Exception
+            Notification that the field is outside limits/set constraints.
+        """
         var_lims = getattr(self, k+"_lim", False)
         if var_lims:
             if not(var_lims[0]<v<var_lims[1]):
@@ -54,16 +105,51 @@ class Parameter(dataobject, readonly=True):
         if var_set:
             if not(v in var_set):
                 raise Exception("Variable "+k+" ("+str(v)+") outside of set constraints: "+str(var_set))
+    def check_immutable(self):
+        """
+        (woefully incomplete) check to ensure defined field values are immutable.
+        Checks if a known/common mutable or a known/common immutable, otherwise 
+        gives a warning.
+
+        Raises
+        ------
+        Exception
+            Throws exception if a known mutable (e.g., dict, set, list, etc)
+        """
+        for f in self.__fields__:
+            attr = getattr(self, f)
+            attr_type = type(attr)
+            if isinstance(attr, (list, set, dict)):
+                raise Exception("Parameter "+f+" type "+str(attr_type)+" is mutable")
+            elif not isinstance(attr, (int, float, tuple, str, Parameter, np.number)):
+                warnings.warn("Parameter "+f+" type "+str(attr_type)+" may be mutable")
+    def check_type(self):
+        """
+        Checks to ensure Parameter type-hints are being followed.
+
+        Raises
+        ------
+        Exception
+            Raises exception if a field is not the same as its defined type.
+        """
+        for typed_field in self.__annotations__:
+            attr_type = type(getattr(self, typed_field))
+            true_type = self.__annotations__.get(typed_field, False)
+            if true_type and not attr_type==true_type:
+                raise Exception(typed_field+" assigned incorrect type: "+str(attr_type)+" (should be "+str(true_type)+")")
+            
         
 
 class Param(Parameter, readonly=True):
+    v:          str 
     x:          float = 30.0
     y:          float = 30.0
-    vel:        float = 0.0
+    vel:        np.float64 = np.float64(0.0)
+    hi = 1
     x_lim = (0.0,100.0)
     y_set = (0.0,30.0,100.0)
 
-a=Param(x=1)
+a=Param(vel=np.float64(1.0), v="hi")
 
 
 
